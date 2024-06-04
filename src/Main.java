7408044c9c4adbc9c378ee9b1cec3685100d1ef3
Milan <133103282+milannal1m@users.noreply.github.com>
2024-06-04 15:02:32 +0200

    Added filter Function Ansatz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -11,5 +12,23 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+    }
+    public JSONArray filter(JSONArray meteors, int minMass, int maxMass){
+
+        JSONArray filteredMeteors = null;
+
+        for (int i = 0; i < meteors.length(); i++) {
+
+            JSONObject meteor = meteors.getJSONObject(i);
+
+            int meteorMass = meteor.getInt("mass");
+
+            if(meteorMass >= minMass && meteorMass <= maxMass){
+                filteredMeteors.put(meteor);
+            }
+        }
+
+        return filteredMeteors;
     }
 }
