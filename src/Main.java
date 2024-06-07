82eb5344dc46377282267f4016bc901d25992087
Milan <133103282+milannal1m@users.noreply.github.com>
2024-06-07 08:18:52 +0200

    All Modules Work

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
 
+        Module module = new Module();
         int numOfParameters = 3;
         String sourcepath = "";
         double minMass = 0.0;
@@ -19,38 +20,27 @@ public class Main {
         } else {
             try {
                 sourcepath = args[0];
-                minMass = Double.parseDouble(args[1]);
+                minMass = Double.parseDouble(args[1]); // Test Eingabe Parameter
                 maxMass = Double.parseDouble(args[2]);
                 String input = new String((Files.readAllBytes(Paths.get(sourcepath))));
                 System.out.print(input);
                 JSONArray meteorJSON = new JSONArray(input);
-                System.out.println(meteorJSON.length());
-                JSONArray filteredMeteorJSON = filter(meteorJSON,minMass,maxMass);
-                System.out.println(filteredMeteorJSON.length());
+
+                System.out.println(meteorJSON.length()); //Anzahl davor
+
+                JSONArray filteredMeteorJSON = module.filter(meteorJSON,44.83333,95.16667,4500); //Region
+                //JSONArray filteredMeteorJSON = module.filter(meteorJSON,300.0,400.0); //Masse
+                //JSONArray filteredMeteorJSON = module.filter(meteorJSON,2000,2011); //Jahr Bereich
+                //JSONArray filteredMeteorJSON = module.filter(meteorJSON,2000,-1); //Jahr
+                //String[] classes = {"H6","L5"}
+                //JSONArray filteredMeteorJSON = module.filter(meteorJSON,44.83333,95.16667,4500); //class
+                System.out.println(filteredMeteorJSON.length()); // Anzahl danach
+
                 //String filteredMeteorString = filteredMeteorJSON.toString(4);
                 //System.out.print(filteredMeteorString);
             } catch (IOException e){
                 System.out.print(e.getMessage());
             }
         }
-    }
-    public static JSONArray filter(JSONArray meteors, double minMass, double maxMass){
-
-        JSONArray filteredMeteors = new JSONArray();
-
-        for (int i = 0; i < meteors.length(); i++) {
-
-            JSONObject meteor = meteors.getJSONObject(i);
-            String meteorMassString = meteor.optString("mass", null);
-            if (meteorMassString != null) {
-                double meteorMass = Double.parseDouble(meteorMassString);
-
-                if (meteorMass >= minMass && meteorMass <= maxMass) {
-                    filteredMeteors.put(meteor);
-                }
-            }
-        }
-
-        return filteredMeteors;
     }
 }
