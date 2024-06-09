a5b86143b8206b649a1284c0d31ff58aeb24dfb1
Milan <133103282+milannal1m@users.noreply.github.com>
2024-06-09 14:21:15 +0200

    Minor Changes

--- src/Main.java
+++ src/Main.java
@@ -19,26 +19,23 @@ public class Main {
         double maxMass = 0.0;
         // weitere Parameter um zu Filtern nötig, numOfParamters muss inkrementiert werden.
 
-        if (args.length < numOfParameters) {
-            System.out.println("Nicht genug Parameter übergeben!");
-        } else {
-            sourcepath = args[0];
-            JSONArray meteorJSON = jsonHandler.JSONParseFile(sourcepath);
-            //minMass = Double.parseDouble(args[1]); // Test Eingabe Parameter
-            //maxMass = Double.parseDouble(args[2]);
 
-            System.out.println(meteorJSON.length()); //Anzahl davor
+        sourcepath = args[0];
+        JSONArray meteorJSON = jsonHandler.JSONParseFile(sourcepath);
+        //minMass = Double.parseDouble(args[1]); // Test Eingabe Parameter
+        //maxMass = Double.parseDouble(args[2]);
 
-            JSONArray filteredMeteorJSON = regionModule.filter(meteorJSON,44.83333,95.16667,4500.0); //Region
-            //JSONArray filteredMeteorJSON = massModule.filter(meteorJSON,300.0,400.0); //Masse
-            //JSONArray filteredMeteorJSON = yearModule.filter(meteorJSON,2000,2011); //Jahr Bereich
-            //JSONArray filteredMeteorJSON = yearModule.filter(meteorJSON,2000); //Jahr
-            //String[] classes = {"H6","L5"};
-            //JSONArray filteredMeteorJSON = classModule.filter(meteorJSON,classes); //class
-            System.out.println(filteredMeteorJSON.length()); // Anzahl danach
+        System.out.println(meteorJSON.length()); //Anzahl davor
 
-            jsonHandler.JSONWrite(filteredMeteorJSON, sourcepath);
+        JSONArray filteredMeteorJSON = regionModule.filter(meteorJSON,44.83333,95.16667,4500.0); //Region
+        //JSONArray filteredMeteorJSON = massModule.filter(meteorJSON,300.0,400.0); //Masse
+        //JSONArray filteredMeteorJSON = yearModule.filter(meteorJSON,2000,2011); //Jahr Bereich
+        //JSONArray filteredMeteorJSON = yearModule.filter(meteorJSON,2000); //Jahr
+        //String[] classes = {"H6","L5"};
+        //JSONArray filteredMeteorJSON = classModule.filter(meteorJSON,classes); //class
+        System.out.println(filteredMeteorJSON.length()); // Anzahl danach
 
+        jsonHandler.JSONWrite(filteredMeteorJSON, sourcepath);
         }
     }
 }
